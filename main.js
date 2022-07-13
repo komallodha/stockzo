1f8a98cfe139b1baf54d04ddb54ea66d9d272175
komal <komallodha9@gmail.com>
2022-07-13 18:52:11 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-components');
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
